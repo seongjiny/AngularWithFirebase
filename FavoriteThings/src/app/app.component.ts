2e862798385c57946a43afbe499eabd48de418d0
Jim Yoon <yoons1@rose-hulman.edu>
2017-06-02 03:01:49 -0400

    basic color change local works

--- FavoriteThings/src/app/app.component.ts
+++ FavoriteThings/src/app/app.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Favorite Things!';
+  favoriteColor = 'pink';
+
+  setColor(selectedColor: string): void {
+    this.favoriteColor = selectedColor
+    console.log('You selected the color ' + selectedColor);
+  }
+
+  updateColor(): void {
+    console.log('You selected update');
+  }
 }
