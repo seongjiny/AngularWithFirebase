d7f7f05c1ede0cd52ad84f0063efef9226f99a04
Jim Yoon <yoons1@rose-hulman.edu>
2017-06-02 16:58:17 -0400

    added table and submit buttonw works

--- MovieQuotes/src/app/app.component.ts
+++ MovieQuotes/src/app/app.component.ts
@@ -11,18 +11,23 @@ interface MovieQuote {
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  movieQuote: any = {
+  formMovieQuote: MovieQuote = {
     'quote': '',
     'movie': ''
   }
   
-  moviequotes: MovieQuote[] = [
+  movieQuotes: MovieQuote[] = [
     { "movie": "Rocky", "quote": "Yo Adrian" },
     { "movie": "Terminator", "quote": "I'll be back" },
     { "movie": "Titanic", "quote": "I'm the king of the world!" },
     { "movie": "The Princess Bride", "quote": "Hello. My name is Inigo Montoya. You killed my father. Prepare to die." }];
   
   onSubmit(): void {
-    console.log("clicked, ",this.movieQuote);
+    this.movieQuotes.unshift(this.formMovieQuote);
+    this.formMovieQuote = {
+      'quote': '',
+      'movie': ''
+    }
   }
+
 }
