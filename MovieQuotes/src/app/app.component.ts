5d95a3e629a497e2a551bcabda6ebb5a42c297d7
Jim Yoon <yoons1@rose-hulman.edu>
2017-06-02 19:05:39 -0400

    Movie Quote all done

--- MovieQuotes/src/app/app.component.ts
+++ MovieQuotes/src/app/app.component.ts
@@ -14,35 +14,47 @@ interface MovieQuote {
 })
 export class AppComponent {
   readonly quotesPath = 'quotes';
+  formMovieQuote: MovieQuote = {
+    'quote': '',
+    'movie': ''
+  }
 
   movieQuotesStream: FirebaseListObservable<MovieQuote[]>;
   constructor(db: AngularFireDatabase) {
     this.movieQuotesStream = db.list(this.quotesPath);
   }
 
-  formMovieQuote: MovieQuote = {
-    'quote': '',
-    'movie': ''
-  }
-/*  // local only
-  movieQuotes: MovieQuote[] = [
-    { "movie": "Rocky", "quote": "Yo Adrian" },
-    { "movie": "Terminator", "quote": "I'll be back" },
-    { "movie": "Titanic", "quote": "I'm the king of the world!" },
-    { "movie": "The Princess Bride", "quote": "Hello. My name is Inigo Montoya. You killed my father. Prepare to die." }];
-*/
+  /*  // local only
+    movieQuotes: MovieQuote[] = [
+      { "movie": "Rocky", "quote": "Yo Adrian" },
+      { "movie": "Terminator", "quote": "I'll be back" },
+      { "movie": "Titanic", "quote": "I'm the king of the world!" },
+      { "movie": "The Princess Bride", "quote": "Hello. My name is Inigo Montoya. You killed my father. Prepare to die." }];
+  */
   onSubmit(): void {
     /*local only*/
-/*    this.movieQuotes.unshift(this.formMovieQuote);*/
+    /*    this.movieQuotes.unshift(this.formMovieQuote);*/
     try {
-      this.movieQuotesStream.push(this.formMovieQuote);
+      if (this.formMovieQuote.$key) {
+        this.movieQuotesStream.update(this.formMovieQuote.$key, this.formMovieQuote);
+      } else {
+        this.movieQuotesStream.push(this.formMovieQuote);
+      }
       this.formMovieQuote = {
         'quote': '',
         'movie': ''
       }
     } catch (e) {
-      console.log("Form error:",e);
+      console.log('Form error:', e);
     }
+  }
+
+  edit(movieQuote: MovieQuote): void {
+    this.formMovieQuote = movieQuote;
+  }
+
+  remove(key: string): void {
+    this.movieQuotesStream.remove(key);
   }
 
 }
--- MovieQuotes/src/environments/environment.ts
+++ MovieQuotes/src/environments/environment.ts
@@ -6,11 +6,11 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: "AIzaSyBDZiq39W1iH8zl1bH2I1ORXVT8Ns2EIPw",
-    authDomain: "yoons1-movie-quotes.firebaseapp.com",
-    databaseURL: "https://yoons1-movie-quotes.firebaseio.com",
-    projectId: "yoons1-movie-quotes",
-    storageBucket: "yoons1-movie-quotes.appspot.com",
-    messagingSenderId: "305202011600"
+    apiKey: 'AIzaSyBDZiq39W1iH8zl1bH2I1ORXVT8Ns2EIPw',
+    authDomain: 'yoons1-movie-quotes.firebaseapp.com',
+    databaseURL: 'https://yoons1-movie-quotes.firebaseio.com',
+    projectId: 'yoons1-movie-quotes',
+    storageBucket: 'yoons1-movie-quotes.appspot.com',
+    messagingSenderId: '305202011600'
   }
 };
