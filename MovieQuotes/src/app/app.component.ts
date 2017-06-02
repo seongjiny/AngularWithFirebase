95f2a55477acacb6328ac61ddd3e89220fc7c374
Jim Yoon <yoons1@rose-hulman.edu>
2017-06-02 16:38:15 -0400

    added bootstrap style and button

--- MovieQuotes/src/app/app.component.ts
+++ MovieQuotes/src/app/app.component.ts
@@ -15,10 +15,14 @@ export class AppComponent {
     'quote': '',
     'movie': ''
   }
+  
   moviequotes: MovieQuote[] = [
     { "movie": "Rocky", "quote": "Yo Adrian" },
     { "movie": "Terminator", "quote": "I'll be back" },
     { "movie": "Titanic", "quote": "I'm the king of the world!" },
     { "movie": "The Princess Bride", "quote": "Hello. My name is Inigo Montoya. You killed my father. Prepare to die." }];
-
+  
+  onSubmit(): void {
+    console.log("clicked, ",this.movieQuote);
+  }
 }
