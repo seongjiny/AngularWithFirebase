722e258ae5fb24271acc6068764a789d28f6e7cb
Jim Yoon <yoons1@rose-hulman.edu>
2017-06-02 15:36:03 -0400

    two data binding done

--- MovieQuotes/src/app/app.component.ts
+++ MovieQuotes/src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+interface MovieQuote {
+  movie: string;
+  quote: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  movieQuote: any = {
+    'quote': '',
+    'movie': ''
+  }
+  moviequotes: MovieQuote[] = [
+    { "movie": "Rocky", "quote": "Yo Adrian" },
+    { "movie": "Terminator", "quote": "I'll be back" },
+    { "movie": "Titanic", "quote": "I'm the king of the world!" },
+    { "movie": "The Princess Bride", "quote": "Hello. My name is Inigo Montoya. You killed my father. Prepare to die." }];
+
 }
