147baaab9f085b65b73ac9699db7dd7d1e495f56
Jim Yoon <yoons1@rose-hulman.edu>
2017-06-30 03:36:24 -0400

    displaycard

--- PasswordKeeper/src/app/password-display-card/password-display-card.component.ts
+++ PasswordKeeper/src/app/password-display-card/password-display-card.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Password } from 'app/modules/password.module';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-password-display-card',
   templateUrl: './password-display-card.component.html',
-  styleUrls: ['./password-display-card.component.scss']
+  styleUrls: ['../shared/common.scss','./password-display-card.component.scss']
 })
 export class PasswordDisplayCardComponent implements OnInit {
-
+  @Input() password: Password;
   constructor() { }
 
   ngOnInit() {
